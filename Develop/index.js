122b5ea9fa391092638e00be4529acac5ff84c37
ian.moss <ianmsieg@gmail.com>
2022-06-05 13:33:50 -0500

    completed questions array

--- Develop/index.js
+++ Develop/index.js
@@ -7,25 +7,84 @@ const inquirer = require('inquirer');
 const questions = [
     {
         type: 'input',
-        message: "What's your name?",
-        name: 'name',
+        name: 'username',
+        message: 'What is your GitHub username?',
+        validate: (ans) => {
+            if(ans !== '') {
+                return true;
+            } else {
+                console.error('Your username cannot be blank');
+                return false;
+            };
+        },
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project called?',
+        validate: (ans) => {
+            if(ans !== '') {
+                return true;
+            } else {
+                console.error('Your project title cannot be blank');
+                return false;
+            };
+        },
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe your project',
+        validate: (ans) => {
+            if(ans !== '') {
+                return true;
+            } else {
+                console.error('Your project description cannot be blank');
+                return false;
+            };
+        },
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Please provide installation instructions or hit enter if there are none',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage instructions or hit enter if there are none',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please provide instructions on how to contribute to this project or hit enter if there are none',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide instructions on how to test your project or hit enter if there are none',
     },
     {
         type: 'list',
-        message: 'Favorite turtle?',
-        name: 'turtles',
-        choices: ['round', 'blue', 'sea', 'ocean']
-    }
+        name: 'license',
+        message: 'Choose a license',
+        choices: ['MIT', 'GNU GPLv3', 'Apache License 2.0', 'ISC License'],
+    },
 ];
 
 inquirer
     .prompt(questions)
-    .then((response) => {
-        const filename = `${response.name.toLowerCase().split(' ').join('')}.md`
+    .then((answers) => {
+        const filename = `${answers.title.toLowerCase().split(' ').join('')}.md`
 
-        fs.writeFile(filename, JSON.stringify(response, null, '\t'), (err) =>
+        fs.writeFile(filename, JSON.stringify(answers, null, '\t'), (err) =>
             err ? console.log(err) : console.log('Success! Check yo files'));
-    });
+    })
 
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
