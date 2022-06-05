33a47fdfa03fef973240aa00f96ee5c2c664e2ab
ian.moss <ianmsieg@gmail.com>
2022-06-05 12:10:20 -0500

    testing inquirer and writefile

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,37 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+// const generate = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: "What's your name?",
+        name: 'name',
+    },
+    {
+        type: 'list',
+        message: 'Favorite turtle?',
+        name: 'turtles',
+        choices: ['round', 'blue', 'sea', 'ocean']
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer
+    .prompt(questions)
+    .then((response) => {
+        const filename = `${response.name.toLowerCase().split(' ').join('')}.md`
 
-// TODO: Create a function to initialize app
-function init() {}
+        fs.writeFile(filename, JSON.stringify(response, null, '\t'), (err) =>
+            err ? console.log(err) : console.log('Success! Check yo files'));
+    });
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
